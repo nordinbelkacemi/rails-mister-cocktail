73324510e3f7b207dfb192eb3c5f50058104d28b
Nordin Belkacemi <belkaceminordin@gmail.com>
2018-08-09 14:46:56 -0400

    list, detail and new cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root directory
+  root to: 'cocktails#index'
+
+  # show wall cocktails in the database
+  get 'cocktails', to: 'cocktails#index', as: 'cocktails'
+
+  # take me back to the /cocktails directory after creating a new cocktail
+  post 'cocktails', to: 'cocktails#create'
+
+  # form to fill out to create new cocktail
+  get 'cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
+
+  # show details of one cocktail
+  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+
+  
 end
